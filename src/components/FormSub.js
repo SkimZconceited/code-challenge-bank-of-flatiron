80fd7f9bac894fa784ef67950dccc0441f0691cf
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-31 20:26:16 +0300

    Data collectors are done

--- src/components/FormSub.js
+++ src/components/FormSub.js
@@ -1,7 +1,25 @@
 function FormSub() {
+
+    const handleSub = (e) => e.preventDefault();
+
   return (
-    <div>
-      <h2>FormSub</h2>
+    <div id="form-Sub">
+      <h2>New Transaction Submission</h2>
+      <form onSubmit={handleSub}>
+        <p>Enter description: <input placeholder="Description..." /></p>
+        <div id="div-category">
+        <p>Choose category:</p>
+
+        <select id="dropdown" name="fruit">
+            <option value="income">Income</option>
+            <option value="fashion">Fashion</option>
+            <option value="food">Food</option>
+            <option value="entertainment">Entertainment</option>
+        </select>
+        </div>
+        <p>Enter amount: <input type="number" placeholder="Amount(kes)..." /></p>
+        <input type="submit" value="Submit Transaction" />
+      </form>
     </div>
   );
 }
